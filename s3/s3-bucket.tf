67099de3617ffa355e3ebbeeaf354cb84c286ac7
timursamanchi <timur.samanchi@gmail.com>
2025-06-03 17:41:44 +0100

    initial push

---
+++ s3/s3-bucket.tf
@@ -0,0 +1,5 @@
+resource "aws_s3_bucket" "my_pub_aws_s3_bucket" {
+    
+
+  
+}
--- s3/variables.tf
+++ s3/variables.tf
@@ -1,4 +1,15 @@
 variable "aws-region" {
+    description = "default aws region"
+    type = string
     default = "eu-west-1"
-    description = "default AWS region"
+}
+variable "bucket-name" {
+    description = "aws s3 bucket name"
+    type = string
+    default = "s3-test-bucket"
+}
+variable "bucket-acl" {
+    description = "the canned acl applies to the bucket"
+    type = string
+    default = "private"
 }
